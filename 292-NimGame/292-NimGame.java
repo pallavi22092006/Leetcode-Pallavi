0a41aeccb0e3c4f9f12e32c940b393a9d669d8e8
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-13 11:31:46 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,12 @@
+// Last updated: 8/13/2026, 11:31:44 AM
+class Solution {
+    public boolean canWinNim(int n) {
+   //int n;
+//    if(n==2)
+//    return true;
+     if(n%4==0)
+     return false;
+     else
+     return true;   
+    }
+}
