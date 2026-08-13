2a058aab4068c344b512f1fefd2d47e742791759
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-13 11:25:05 +0530

    Create 958-SortArrayByParityIi

---
+++ 958-SortArrayByParityIi/958-SortArrayByParityIi.java
@@ -0,0 +1,20 @@
+// Last updated: 8/13/2026, 11:25:03 AM
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int i=0, j=1;
+        while(i<nums.length && j<nums.length){
+            while(i<nums.length && nums[i]%2==0){
+                i+=2;
+            }
+            while(j<nums.length && nums[j]%2==1){
+                j+=2;
+            }
+            if(i<nums.length && j<nums.length){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+        }
+        return nums;
+    }
+}
