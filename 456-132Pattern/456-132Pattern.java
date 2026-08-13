e3b919d7d92f634144db12d2af5a54e0c8ee9747
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-13 11:29:46 +0530

    Create 456-132Pattern

---
+++ 456-132Pattern/456-132Pattern.java
@@ -0,0 +1,15 @@
+// Last updated: 8/13/2026, 11:29:44 AM
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> st=new Stack<>();
+        int third=Integer.MIN_VALUE;
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]<third)
+               return true;
+            while(!st.isEmpty() && nums[i]>st.peek())
+               third=st.pop();
+            st.push(nums[i]);
+        }
+        return false;
+    }
+}
