8ac4aa9d7d99fdbe9f13eff48c50bddfcb2adf2e
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-13 11:27:24 +0530

    Create 647-PalindromicSubstrings

---
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -0,0 +1,33 @@
+// Last updated: 8/13/2026, 11:27:22 AM
+class Solution {
+
+    public int countSubstrings(String s) {
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            count += expand(s, i, i);
+
+            count += expand(s, i, i + 1);
+        }
+
+        return count;
+    }
+
+    private int expand(String s, int left, int right) {
+
+        int count = 0;
+
+        while (left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
+
+            count++;
+
+            left--;
+            right++;
+        }
+
+        return count;
+    }
+}
