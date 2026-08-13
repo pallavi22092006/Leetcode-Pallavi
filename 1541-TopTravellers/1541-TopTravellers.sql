f22eec339d7a8ff4f4b0589047cb80d3f7201131
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-13 11:22:28 +0530

    Create 1541-TopTravellers

---
+++ 1541-TopTravellers/1541-TopTravellers.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/13/2026, 11:22:26 AM
+# Write your MySQL query statement below
+SELECT 
+    u.name,
+    COALESCE(SUM(r.distance), 0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name ASC;
