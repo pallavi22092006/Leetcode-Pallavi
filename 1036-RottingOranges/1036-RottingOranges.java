176fc63f780b846ee2eaf5ea9ef49a8bcaa234fc
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-13 11:24:36 +0530

    Create 1036-RottingOranges

---
+++ 1036-RottingOranges/1036-RottingOranges.java
@@ -0,0 +1,55 @@
+// Last updated: 8/13/2026, 11:24:34 AM
+import java.util.*;
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+
+        // Find all rotten oranges and count fresh oranges
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        if (fresh == 0) return 0;
+
+        int time = 0;
+        int[][] dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        while (!q.isEmpty() && fresh > 0) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+
+                for (int[] d : dir) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+
+                    if (nr >= 0 && nr < rows &&
+                        nc >= 0 && nc < cols &&
+                        grid[nr][nc] == 1) {
+
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.offer(new int[]{nr, nc});
+                    }
+                }
+            }
+            time++;
+        }
+
+        return fresh == 0 ? time : -1;
+    }
+}
