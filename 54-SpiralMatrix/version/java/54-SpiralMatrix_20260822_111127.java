7ef5b28ebb03d230344c07b849f9502e32fd885c
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-22 11:11:27 +0530

    Create 54-SpiralMatrix

---
+++ 54-SpiralMatrix/version/java/54-SpiralMatrix_20260822_111127.java
@@ -0,0 +1,24 @@
+// Last updated: 8/22/2026, 11:11:27 AM
+1class Solution {
+2    public int[] productExceptSelf(int[] nums) {
+3
+4        int n = nums.length;
+5        int[] ans = new int[n];
+6
+7        int prefix = 1;
+8
+9        for (int i = 0; i < n; i++) {
+10            ans[i] = prefix;
+11            prefix *= nums[i];
+12        }
+13
+14        int suffix = 1;
+15
+16        for (int i = n - 1; i >= 0; i--) {
+17            ans[i] *= suffix;
+18            suffix *= nums[i];
+19        }
+20
+21        return ans;
+22    }
+23}
