aba0eba38737d1627229ab32efdf3c6ad162ac1a
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-22 10:02:39 +0530

    Create 3016-MinimumNumberofPushestoTypeWordII

---
+++ 3016-MinimumNumberofPushestoTypeWordII/version/java/3016-MinimumNumberofPushestoTypeWordII_20260822_100238.java
@@ -0,0 +1,28 @@
+// Last updated: 8/22/2026, 10:02:38 AM
+1import java.util.*;
+2
+3class Solution {
+4    public int[] topKFrequent(int[] nums, int k) {
+5        HashMap<Integer, Integer> map = new HashMap<>();
+6
+7        for (int n : nums)
+8            map.put(n, map.getOrDefault(n, 0) + 1);
+9
+10        PriorityQueue<Integer> pq =
+11            new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+12
+13        for (int n : map.keySet()) {
+14            pq.offer(n);
+15
+16            if (pq.size() > k)
+17                pq.poll();
+18        }
+19
+20        int[] ans = new int[k];
+21
+22        for (int i = 0; i < k; i++)
+23            ans[i] = pq.poll();
+24
+25        return ans;
+26    }
+27}
