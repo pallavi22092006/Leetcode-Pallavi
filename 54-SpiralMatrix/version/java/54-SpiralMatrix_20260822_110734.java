8243179cd9bf504e7504eb6ed51c591a19ece3cd
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-22 11:07:34 +0530

    Create 54-SpiralMatrix

---
+++ 54-SpiralMatrix/version/java/54-SpiralMatrix_20260822_110734.java
@@ -0,0 +1,22 @@
+// Last updated: 8/22/2026, 11:07:34 AM
+1class Solution {
+2    public int findDuplicate(int[] nums) {
+3
+4        int slow = nums[0];
+5        int fast = nums[0];
+6
+7        do {
+8            slow = nums[slow];
+9            fast = nums[nums[fast]];
+10        } while (slow != fast);
+11
+12        slow = nums[0];
+13
+14        while (slow != fast) {
+15            slow = nums[slow];
+16            fast = nums[fast];
+17        }
+18
+19        return slow;
+20    }
+21}
