192c29051a2d440b0233976cbf1e6972e59731a0
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-22 10:01:36 +0530

    Create 3016-MinimumNumberofPushestoTypeWordII

---
+++ 3016-MinimumNumberofPushestoTypeWordII/version/java/3016-MinimumNumberofPushestoTypeWordII_20260822_100135.java
@@ -0,0 +1,27 @@
+// Last updated: 8/22/2026, 10:01:35 AM
+1import java.util.*;
+2
+3class Solution {
+4    public int fourSumCount(int[] nums1, int[] nums2,
+5                            int[] nums3, int[] nums4) {
+6
+7        HashMap<Integer, Integer> map = new HashMap<>();
+8
+9        for (int a : nums1) {
+10            for (int b : nums2) {
+11                int sum = a + b;
+12                map.put(sum, map.getOrDefault(sum, 0) + 1);
+13            }
+14        }
+15
+16        int ans = 0;
+17
+18        for (int c : nums3) {
+19            for (int d : nums4) {
+20                ans += map.getOrDefault(-(c + d), 0);
+21            }
+22        }
+23
+24        return ans;
+25    }
+26}
