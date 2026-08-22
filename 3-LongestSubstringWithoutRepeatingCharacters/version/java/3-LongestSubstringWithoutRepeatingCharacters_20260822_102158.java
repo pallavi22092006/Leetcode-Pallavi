465df88302580d73802f4e579a9ddfdfd631023e
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-22 10:21:58 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/version/java/3-LongestSubstringWithoutRepeatingCharacters_20260822_102158.java
@@ -0,0 +1,19 @@
+// Last updated: 8/22/2026, 10:21:58 AM
+1class Solution {
+2    public boolean hasAlternatingBits(int n) {
+3        int prev = n & 1;
+4        n >>= 1;
+5
+6        while (n > 0) {
+7            int current = n & 1;
+8
+9            if (current == prev)
+10                return false;
+11
+12            prev = current;
+13            n >>= 1;
+14        }
+15
+16        return true;
+17    }
+18}
