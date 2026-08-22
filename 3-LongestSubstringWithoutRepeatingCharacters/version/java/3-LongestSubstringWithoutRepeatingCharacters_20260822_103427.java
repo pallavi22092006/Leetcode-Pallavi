3a7965999a2a08363346acaa8f761dedf91dc675
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-22 10:34:27 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/version/java/3-LongestSubstringWithoutRepeatingCharacters_20260822_103427.java
@@ -0,0 +1,33 @@
+// Last updated: 8/22/2026, 10:34:27 AM
+1import java.util.*;
+2
+3class Solution {
+4    public List<List<Integer>> findWinners(int[][] matches) {
+5
+6        HashMap<Integer, Integer> losses = new HashMap<>();
+7
+8        for (int[] match : matches) {
+9            int winner = match[0];
+10            int loser = match[1];
+11
+12            losses.putIfAbsent(winner, 0);
+13            losses.put(loser, losses.getOrDefault(loser, 0) + 1);
+14        }
+15
+16        List<Integer> zero = new ArrayList<>();
+17        List<Integer> one = new ArrayList<>();
+18
+19        for (int player : losses.keySet()) {
+20
+21            if (losses.get(player) == 0)
+22                zero.add(player);
+23            else if (losses.get(player) == 1)
+24                one.add(player);
+25        }
+26
+27        Collections.sort(zero);
+28        Collections.sort(one);
+29
+30        return Arrays.asList(zero, one);
+31    }
+32}
