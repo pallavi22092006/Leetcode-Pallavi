e061d4785dd451f0257a6b011c417ce4a6a58876
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-28 09:31:15 +0530

    Create 901-OnlineStockSpan

---
+++ 901-OnlineStockSpan/version/java/901-OnlineStockSpan_20260828_093109.java
@@ -0,0 +1,18 @@
+// Last updated: 8/28/2026, 9:31:09 AM
+1public class Solution extends VersionControl {
+2    public int firstBadVersion(int n) {
+3        int low = 1, high = n;
+4
+5        while (low < high) {
+6            int mid = low + (high - low) / 2;
+7
+8            if (isBadVersion(mid)) {
+9                high = mid;
+10            } else {
+11                low = mid + 1;
+12            }
+13        }
+14
+15        return low;
+16    }
+17}
