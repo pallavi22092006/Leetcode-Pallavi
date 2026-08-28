8efabdb6d28115924bbfe5c3ca16688751f65806
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-28 09:41:56 +0530

    Create 901-OnlineStockSpan

---
+++ 901-OnlineStockSpan/version/java/901-OnlineStockSpan_20260828_094151.java
@@ -0,0 +1,19 @@
+// Last updated: 8/28/2026, 9:41:51 AM
+1class Solution {
+2    public int findMin(int[] nums) {
+3        int low = 0;
+4        int high = nums.length - 1;
+5
+6        while (low < high) {
+7            int mid = low + (high - low) / 2;
+8
+9            if (nums[mid] > nums[high]) {
+10                low = mid + 1;
+11            } else {
+12                high = mid;
+13            }
+14        }
+15
+16        return nums[low];
+17    }
+18}
