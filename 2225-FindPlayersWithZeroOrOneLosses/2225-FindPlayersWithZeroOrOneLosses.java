6d5eb5f5ddd11cd69823049ab4c65d01c1e8a1c9
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:18:38 +0530

    Create 2225-FindPlayersWithZeroOrOneLosses

---
+++ 2225-FindPlayersWithZeroOrOneLosses/2225-FindPlayersWithZeroOrOneLosses.java
@@ -0,0 +1,33 @@
+// Last updated: 9/15/2026, 4:18:35 PM
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> findWinners(int[][] matches) {
+
+        HashMap<Integer, Integer> losses = new HashMap<>();
+
+        for (int[] match : matches) {
+            int winner = match[0];
+            int loser = match[1];
+
+            losses.putIfAbsent(winner, 0);
+            losses.put(loser, losses.getOrDefault(loser, 0) + 1);
+        }
+
+        List<Integer> zero = new ArrayList<>();
+        List<Integer> one = new ArrayList<>();
+
+        for (int player : losses.keySet()) {
+
+            if (losses.get(player) == 0)
+                zero.add(player);
+            else if (losses.get(player) == 1)
+                one.add(player);
+        }
+
+        Collections.sort(zero);
+        Collections.sort(one);
+
+        return Arrays.asList(zero, one);
+    }
+}
