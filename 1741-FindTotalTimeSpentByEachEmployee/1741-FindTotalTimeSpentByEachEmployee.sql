c2706f79fc26203b3dd67ac9a48d379e0d2a185a
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:17:13 +0530

    Create 1741-FindTotalTimeSpentByEachEmployee

---
+++ 1741-FindTotalTimeSpentByEachEmployee/1741-FindTotalTimeSpentByEachEmployee.sql
@@ -0,0 +1,7 @@
+-- Last updated: 9/15/2026, 4:17:10 PM
+# Write your MySQL query statement below
+SELECT event_day AS day,
+       emp_id,
+       SUM(out_time - in_time) AS total_time
+FROM Employees
+GROUP BY event_day, emp_id;
