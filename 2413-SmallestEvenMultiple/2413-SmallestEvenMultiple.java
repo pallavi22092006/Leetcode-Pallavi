bbf77623ad5f1e4a0cf7134762c759367ed736c4
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:16:27 +0530

    Create 2413-SmallestEvenMultiple

---
+++ 2413-SmallestEvenMultiple/2413-SmallestEvenMultiple.java
@@ -0,0 +1,9 @@
+// Last updated: 9/15/2026, 4:16:25 PM
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0 )
+        return n;
+        else
+        return n*2;
+    }
+}
