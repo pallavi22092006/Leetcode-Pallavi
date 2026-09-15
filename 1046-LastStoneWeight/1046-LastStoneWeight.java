471f012d85ed953c5b921203b074d29a4fdd308d
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:19:40 +0530

    Create 1046-LastStoneWeight

---
+++ 1046-LastStoneWeight/1046-LastStoneWeight.java
@@ -0,0 +1,21 @@
+// Last updated: 9/15/2026, 4:19:37 PM
+import java.util.PriorityQueue;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int stone : stones)
+            pq.offer(stone);
+
+        while (pq.size() > 1) {
+            int x = pq.poll();
+            int y = pq.poll();
+
+            if (x != y)
+                pq.offer(x - y);
+        }
+
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
