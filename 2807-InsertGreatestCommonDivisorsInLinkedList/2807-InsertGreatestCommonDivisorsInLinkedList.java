78b8b825cf7990ea9fa74f0653e73af8e2e297b5
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:15:38 +0530

    Create 2807-InsertGreatestCommonDivisorsInLinkedList

---
+++ 2807-InsertGreatestCommonDivisorsInLinkedList/2807-InsertGreatestCommonDivisorsInLinkedList.java
@@ -0,0 +1,35 @@
+// Last updated: 9/15/2026, 4:15:35 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode curr=head;
+        while(curr!=null && curr.next!=null){
+            int gcd=gcd(curr.val,curr.next.val);
+            ListNode nn=new ListNode(gcd);
+            // to build the connection,stores next node 
+            nn.next=curr.next;
+            curr.next=nn;
+            //to process next pair
+            curr=nn.next;
+        }
+        return head;
+    }
+        private int gcd(int a,int b){
+        while(b>0){
+            int temp=a%b;
+            a=b;
+            b=temp;
+        }
+        return a;
+    }
+}
