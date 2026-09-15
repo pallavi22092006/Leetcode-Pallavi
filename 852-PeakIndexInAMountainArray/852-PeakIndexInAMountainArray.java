3fe14eb75100bda312103590eefb98c12a82e206
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:21:00 +0530

    Create 852-PeakIndexInAMountainArray

---
+++ 852-PeakIndexInAMountainArray/852-PeakIndexInAMountainArray.java
@@ -0,0 +1,14 @@
+// Last updated: 9/15/2026, 4:20:57 PM
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left=0,right=arr.length-1;
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(arr[mid]<arr[mid+1])
+               left=mid+1;
+            else
+               right=mid;
+        }
+        return left;
+    }
+}
