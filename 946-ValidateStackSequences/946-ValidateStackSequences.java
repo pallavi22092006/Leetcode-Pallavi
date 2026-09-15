3553e0ab455d99253cc6dc0ee3fe0e6c4904007b
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:20:13 +0530

    Create 946-ValidateStackSequences

---
+++ 946-ValidateStackSequences/946-ValidateStackSequences.java
@@ -0,0 +1,23 @@
+// Last updated: 9/15/2026, 4:20:10 PM
+import java.util.*;
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+
+        int j = 0;
+
+        for (int x : pushed) {
+            stack.push(x);
+
+            while (!stack.isEmpty() && 
+                   j < popped.length && 
+                   stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
