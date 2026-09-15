4c52d55fae3020da67810127d4f4d08e395c5714
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:21:31 +0530

    Create 771-JewelsAndStones

---
+++ 771-JewelsAndStones/771-JewelsAndStones.java
@@ -0,0 +1,18 @@
+// Last updated: 9/15/2026, 4:21:29 PM
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> set=new HashSet<>();
+
+        for(char c:jewels.toCharArray())
+            set.add(c);
+
+        int count=0;
+
+        for(char c:stones.toCharArray()){
+            if(set.contains(c)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
