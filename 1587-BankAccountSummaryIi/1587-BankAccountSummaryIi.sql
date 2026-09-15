0bf3b8ce2a64c29333176adbf00914565080cc4e
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:17:33 +0530

    Create 1587-BankAccountSummaryIi

---
+++ 1587-BankAccountSummaryIi/1587-BankAccountSummaryIi.sql
@@ -0,0 +1,10 @@
+-- Last updated: 9/15/2026, 4:17:30 PM
+# Write your MySQL query statement below
+SELECT
+    u.name,
+    SUM(t.amount) AS balance
+FROM Users u
+JOIN Transactions t
+ON u.account = t.account
+GROUP BY u.account, u.name
+HAVING SUM(t.amount) > 10000;
