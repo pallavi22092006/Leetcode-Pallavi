e600dd471bd2302b0af1cbd8db3eb0d72204606b
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:19:25 +0530

    Create 1070-ProductSalesAnalysisIii

---
+++ 1070-ProductSalesAnalysisIii/1070-ProductSalesAnalysisIii.sql
@@ -0,0 +1,12 @@
+-- Last updated: 9/15/2026, 4:19:22 PM
+SELECT
+    product_id,
+    year AS first_year,
+    quantity,
+    price
+FROM Sales
+WHERE (product_id, year) IN (
+    SELECT product_id, MIN(year)
+    FROM Sales
+    GROUP BY product_id
+);
