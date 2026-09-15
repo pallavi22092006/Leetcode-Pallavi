8b148a7520f535820f14995ffb9b874ff9768232
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:20:17 +0530

    Create 933-NumberOfRecentCalls

---
+++ 933-NumberOfRecentCalls/933-NumberOfRecentCalls.java
@@ -0,0 +1,21 @@
+// Last updated: 9/15/2026, 4:20:14 PM
+import java.util.*;
+
+class RecentCounter {
+
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
