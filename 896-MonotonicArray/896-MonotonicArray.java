fad3aaa104611fa7a7479ad7827d1277e9e955a6
pallavi22092006 <pallavigowda0169@gmail.com>
2026-09-15 16:20:43 +0530

    Create 896-MonotonicArray

---
+++ 896-MonotonicArray/896-MonotonicArray.java
@@ -0,0 +1,12 @@
+// Last updated: 9/15/2026, 4:20:41 PM
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean inc=true,dec=true;
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] > nums[i-1])  dec=false;
+            if(nums[i] < nums[i-1])  inc=false;
+        }
+        return inc || dec;
+    }
+}
